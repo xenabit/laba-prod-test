7c76cd21d9e9612e1e94685140adbce42c317ecf
Xenia <k.nikolaevna98@gmail.com>
2026-04-07 21:20:45 +0300

    вапвап

---
+++ assets/case-compare-slider-bg-hr-1-C69Q6hCG.js
@@ -0,0 +1,1 @@
+const e=""+new URL("images/case-compare-slider-bg-hr-1-Ci9W1ca_.svg",import.meta.url).href;export{e as c};
--- assets/case-compare-slider-bg-hr-1-SnlyAtNM.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("images/case-compare-slider-bg-hr-1-Cg2pw7es.svg",import.meta.url).href;export{e as c};
